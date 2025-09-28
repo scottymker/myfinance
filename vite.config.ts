b8f430e13089f1706c45d028455a40e123dfcdfa
scottymker <scott@scottymkerphotos.com>
2025-09-28 09:00:20 +0000

    Enable prod sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({ build: { sourcemap: true },
+export default defineConfig({ build: { sourcemap: true }, build: { sourcemap: true },
   plugins: [react()],
   server: { host: true } // allow external access in Codespaces
 })
